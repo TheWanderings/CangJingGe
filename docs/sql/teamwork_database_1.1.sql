317124512400e3ea6f52a6011b55ef92ceab5d17
aodiwei <adwdavid@163.com>
2016-04-09 16:16:31 +0800

    fix some error, passed test in local

--- docs/sql/teamwork_database_1.1.sql
+++ docs/sql/teamwork_database_1.1.sql
@@ -1,17 +1,12 @@
-
-DROP DATABASE IF EXISTS teamwork;
--- database
-CREATE DATABASE IF NOT EXISTS teamwork;
-USE teamwork;
-
 -- 用户表
+SET FOREIGN_KEY_CHECKS=0; -- 禁用外键约束，创建完后恢复
 DROP TABLE IF EXISTS tbl_user;
 CREATE TABLE tbl_user(
 	uid INT PRIMARY KEY,
-	account VARCHAR UNIQUE NOT NULL,
+	account VARCHAR(255) UNIQUE NOT NULL,
 	email CHAR(50) UNIQUE NOT NULL,
 	password CHAR(50) NOT NULL,
-	image VARCHAR,
+	image VARCHAR(255),
 	create_time DATETIME
 	
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -28,12 +23,12 @@ DELIMITER ;
 DROP TABLE IF EXISTS tbl_project;
 CREATE TABLE tbl_project(
 	pid INT PRIMARY KEY,
-	name VARCHAR UNIQUE NOT NULL,
+	name VARCHAR(255) UNIQUE NOT NULL,
 	uid INT NOT NULL,  -- 文档中的owner，我认为名字和依赖表一致方便查询
-	description VARCHAR NOT NULL
-	image VARCHAR,
+	description VARCHAR(255) NOT NULL,
+	image VARCHAR(255),
 	create_time DATETIME,
-	FOREIGN KEY uid REFERENCES tbl_user ON DELETE CASCADE	
+	FOREIGN KEY (uid) REFERENCES tbl_user(uid) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELIMITER |   
@@ -49,10 +44,10 @@ DROP TABLE IF EXISTS tbl_function;
 CREATE TABLE tbl_function(
 	fid INT PRIMARY KEY,
 	pid INT NOT NULL,
-	name VARCHAR NOT NULL,
-	description VARCHAR NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
 	create_time DATETIME,
-	FOREIGN KEY pid REFERENCES tbl_project ON DELETE CASCADE	
+	FOREIGN KEY (pid) REFERENCES tbl_project(pid) ON DELETE CASCADE ON UPDATE CASCADE	
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELIMITER |   
@@ -68,14 +63,15 @@ DROP TABLE IF EXISTS tbl_step;
 CREATE TABLE tbl_step(
 	sid INT PRIMARY KEY,
 	fid INT NOT NULL,
-	name VARCHAR NOT NULL,
-	description VARCHAR NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
 	work_time LONG NOT NULL,
-	status ENUM NOT NULL,
+	status CHAR(50) NOT NULL,
+	progress INT DEFAULT 0 NOT NULL, -- 进度，完成百分比
 	deadline DATETIME,
-	requirement VARCHAR,
+	requirement VARCHAR(255),
 	create_time DATETIME,
-	FOREIGN KEY fid REFERENCES tbl_function ON DELETE CASCADE	
+	FOREIGN KEY (fid) REFERENCES tbl_function(fid) ON DELETE CASCADE ON UPDATE CASCADE	
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELIMITER |   
@@ -90,11 +86,11 @@ DELIMITER ;
 DROP TABLE IF EXISTS tbl_comment;
 CREATE TABLE tbl_comment(
 	cid INT PRIMARY KEY,
-	type ENUM NOT NULL, -- 记录是对工程的还是功能，步骤的评论, 记为实体
+	type INT NOT NULL, -- 记录是对工程的还是功能，步骤的评论, 记为实体 1--工程，2--功能， 3--步骤
 	ref_id INT NOT NULL, -- 实体id
-	context VARCHAR,
-	create_time DATETIME,
-	FOREIGN KEY fid REFERENCES tbl_function ON DELETE CASCADE	
+	replay_id INT DEFAULT 0, -- 回复评论 cid， 默认0表示原始评论，其他为回复对象，
+	context VARCHAR(255),
+	create_time DATETIME
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELIMITER |   
@@ -112,13 +108,11 @@ DELIMITER ;
 -- 我不知道你的业务里是不是工程->功能->步骤 这样一条线下来的
 DROP TABLE IF EXISTS tbl_work_staff;
 CREATE TABLE tbl_work_staff(
-	pid INT NOT NULL,
-	fid INT NOT NULL,
 	sid INT NOT NULL,
 	staff INT NOT NULL, -- uid
 	PRIMARY KEY(sid, staff),
-	FOREIGN KEY staff REFERENCES tbl_user ON DELETE CASCADE,
-	FOREIGN KEY pid REFERENCES tbl_project ON DELETE CASCADE,
-	FOREIGN KEY fid REFERENCES tbl_function ON DELETE CASCADE,
-	FOREIGN KEY sid REFERENCES tbl_step ON DELETE CASCADE	
+	FOREIGN KEY (staff) REFERENCES tbl_user(uid) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (sid) REFERENCES tbl_step(sid) ON DELETE CASCADE ON UPDATE CASCADE	
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS=1; -- 恢复外键约束
